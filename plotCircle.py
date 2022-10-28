de1adb52f20258e4fadf5522d42b3b0ed54be1d2
TDGROUP\c.vitiello <c.vitiello@tdnet.it>
2022-10-28 15:07:46 +0200

    miglioria plot cerchi

--- plotCircle.py
+++ plotCircle.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from attr import dataclass
+from matplotlib.axes import Axes
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,40 +17,44 @@ class Orologio():
     title: str
 
 def letturaConfig(f) -> list[Orologio]:
-    dict = {}
+    dictDati = {}
     listaOrologi = []
     for line in f.readlines():
+        line = line.replace("\n","")
         if(line.startswith('##!')):
-            orologio = Orologio(dict['colors'],
-                int(dict['total']),int(dict['colored']),dict['title'])
+            orologio = Orologio(dictDati['color'],
+                int(dictDati['total']),int(dictDati['colored']),dictDati['title'])
             listaOrologi.append(orologio)
         if(not line.startswith('#')):
-            dict[line.split()[0]] = line.split()[-1]
+            text = ''
+            line = line.split()
+            for word in line:
+                if(line.index(word) > 1):
+                    text += word
+                    if(len(line) > 3): # nomeValore = Valore
+                        text += ' '
+            dictDati[line[0]] = text
     # print(dict)
     return listaOrologi
 
 def scritturaConfig(f,listaOrologi: list) -> None:
     for orologio in listaOrologi:
-        f.write(f"colors = {orologio.colors}")
+        f.write(f"color = {orologio.color}")
         f.write(f"total = {orologio.total}")
         f.write(f"colored = {orologio.colored}")
         f.write(f"title = {orologio.title}")
         f.write(f"##!")
 
-def mypie(ax,colors,total,colored,title):
-    global numberCircle
-    slices = []
-    for i in range(total):
-        slices.append(1)
-    pie_wedge_collection = ax[numberCircle].pie(slices, labeldistance=1.05)#labels=labels, autopct=make_autopct(slices))
+def mypie(ax:list[Axes],color: str,total: int,colored: int,title: str,numeroOrologi :int):
+    slices = [1 for i in range(total)]
+    pie_wedge_collection = ax[numeroOrologi].pie(slices, labeldistance=1.05)#labels=labels, autopct=make_autopct(slices))
 
-    ax[numberCircle].set_title(title)
+    ax[numeroOrologi].set_title(title)
 
-    numberCircle += 1
     count = 0
     for pie_wedge in pie_wedge_collection[0]:
         if(count < colored):
-            pie_wedge.set_facecolor(colors[0])
+            pie_wedge.set_facecolor(color)
         else:
             pie_wedge.set_facecolor('white')
         count += 1
@@ -58,33 +63,15 @@ def mypie(ax,colors,total,colored,title):
 
     return ax,pie_wedge_collection
 
-
-
-
-
-# slices = [37, 39, 39] #, 38, 62, 21, 15,  9,  6,  7,  6,  5,  4, 3
-# cmap = plt.cm.prism
-# colors = cmap(np.linspace(0., 1., len(slices)))
-# labels = [u'TI', u'Con', u'FR'] #, u'TraI', u'Bug', u'Data', u'Int', u'KB', u'Other', u'Dep', u'PW', u'Uns', u'Perf', u'Dep'
-
-f = open(path,'rt')
-
-listOrologi = letturaConfig(f)
-if(listOrologi != []):
-    print(listOrologi)
-    numeroOrologi = len(listOrologi)
-    print(f"ci sono a disposizione già {numeroOrologi} orologi")
-    risposta = input("Vuoi iniziare da zero? s/n\n")
-    if(risposta.lower() == "n"):
+def main():    
+    while True:
+        with open(path,'rt') as f:
+            listOrologi = letturaConfig(f)
+            print (listOrologi)
+        numeroOrologi = len(listOrologi)
         fig,ax = plt.subplots(1,numeroOrologi)
         for orologio in listOrologi:
-            mypie(ax,orologio.color,orologio.total,orologio.colored,orologio.title)  
-else:
-    print(f"vuota {listOrologi}")
-# fig,ax = plt.subplots(1,5)
-# colors = ['red']
-
-# mypie(ax,colors,10,1,"battaglia")
-# mypie(ax,colors,10,3,"caduta")
-
-plt.show()
+            mypie(ax,orologio.color,orologio.total,orologio.colored,orologio.title, numeroOrologi - 1)
+            numeroOrologi -= 1
+        plt.show()
+main()
